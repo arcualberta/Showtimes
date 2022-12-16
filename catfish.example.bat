7ecde8f461ddd18f809546f6ad054b47f0dfb548
KamalRanaweera <ranaweer@ualberta.ca>
2022-12-16 11:55:58 -0700

    Updated the catfish.example.bat

--- catfish.example.bat
+++ catfish.example.bat
@@ -1,7 +1,7 @@
 :: Run this batch file as: cmd /c catfish.bat
 :: Before run it, change the CATFISH_UI_LIB_PATH according to your local settings
 
-set CATFISH_UI_LIB_PATH = "../Catfish3-workflow/catfish-ui/"
-set SHOWTIMES_PATH ="."
+set CATFISH_UI_LIB_PATH=../Catfish-3.0/catfish-ui/
+set SHOWTIMES_PATH_FROM_CATFISH_UI=../../Showtimes
 
-cd CATFISH_UI_LIB_PATH && npm run build && npm pack && cd "../../Showtimes" && npm install CATFISH_UI_LIB_PATH\arcualberta-catfish-ui-1.0.0.tgz
+cd %CATFISH_UI_LIB_PATH% && npm run build && npm pack && cd %SHOWTIMES_PATH_FROM_CATFISH_UI% && npm install %CATFISH_UI_LIB_PATH%\applets-0.0.0.tgz
