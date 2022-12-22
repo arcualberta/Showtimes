f10786ca29f7967dee7db9d91445e668936cc8f2
KamalRanaweera <ranaweer@ualberta.ca>
2022-12-21 23:24:19 -0700

    Added appsettings.ts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 //import {SolrSerachPanel} from ""
+import {solrFields, queryApi} from '@/appsettings'
 
 </script>
 
 <template>
   <h1>Home</h1>
 
-   <SolrSearchPanel />
+   <SolrSearchPanel :search-fields="solrFields" :query-api="queryApi" />
 </template>
