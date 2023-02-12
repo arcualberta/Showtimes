7636e58c36b329ba4c76450e501381aca83be205
KamalRanaweera <ranaweer@ualberta.ca>
2023-02-11 17:13:15 -0700

    Updated catfish.example.bat file.

--- catfish.example.bat
+++ catfish.example.bat
@@ -1,7 +1,6 @@
-:: Run this batch file as: cmd /c catfish.bat
-:: Before run it, change the CATFISH_UI_LIB_PATH according to your local settings
+:: Before run this batch file, change the CATFISH_UI_LIB_PATH according to your local settings
 
 set CATFISH_UI_LIB_PATH=../Catfish-3.0/catfish-ui/
 set SHOWTIMES_PATH_FROM_CATFISH_UI=../../Showtimes
 
-cd %CATFISH_UI_LIB_PATH% && npm run build && npm pack && cd %SHOWTIMES_PATH_FROM_CATFISH_UI% && npm install %CATFISH_UI_LIB_PATH%\applets-0.0.0.tgz
+cmd /c "cd %CATFISH_UI_LIB_PATH% && npm run build && npm pack && cd %SHOWTIMES_PATH_FROM_CATFISH_UI% && npm install %CATFISH_UI_LIB_PATH%\applets-0.0.0.tgz"
